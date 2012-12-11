7d61a5cbc2dc8b32e3fd5cdb341d49df84fdf608
Sh4d <aniellob@gmail.com>
2012-12-11 15:42:19 +0100

    Update celery_admin/admin.py
    now it's possible to pass args & kwargs through admin panel :1

--- celery_admin/admin.py
+++ celery_admin/admin.py
@@ -2,6 +2,7 @@ from celery.execute import send_task
 from django.contrib import admin
 from djcelery import admin as djcelery_admin
 from djcelery.models import PeriodicTask
+import json
 
 class ExtendedPeriodicTaskAdmin(djcelery_admin.PeriodicTaskAdmin):
     actions = djcelery_admin.PeriodicTaskAdmin.actions + ['run_task']
@@ -9,7 +10,7 @@ class ExtendedPeriodicTaskAdmin(djcelery_admin.PeriodicTaskAdmin):
     def run_task(self, request, queryset):
         if request.user.is_superuser:
             for task in queryset.all():
-                send_task(task.task)
+                send_task(task.task, args=json.loads(task.args), kwargs=json.loads(task.kwargs))
             self.message = 'Tasks are running'
         else:
             self.message = 'You must be a superuser to perform this action.'
